4ba48ddbbd4933297dd34af9dd6b8f3fd97504cf
Naglaa Raslan <naglaa.raslan@gmail.com>
2021-10-04 18:07:51 +0200

    Add files via upload

---
+++ square cards/script.js
@@ -0,0 +1,24 @@
+let openVideos = document.querySelectorAll(".top .link-img");
+let openLinks = document.querySelectorAll(".top .link-btn");
+
+openVideos[0].addEventListener("click", () => {
+  window.open("https://youtu.be/3go6fX-bvp8");
+});
+
+openVideos[1].addEventListener("click", () => {
+  window.open("https://youtu.be/tO5QoRo2q8M");
+});
+
+openVideos[2].addEventListener("click", () => {
+  window.open("https://youtu.be/6Ire17yjpQ8");
+});
+
+openLinks[0].addEventListener("click", () => {
+  window.open("https://mint.intuit.com/");
+});
+openLinks[1].addEventListener("click", () => {
+  window.open("https://www.spotify.com/eg-en/");
+});
+openLinks[2].addEventListener("click", () => {
+  window.open("https://www.plex.tv/");
+});
